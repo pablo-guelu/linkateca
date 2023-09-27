0e0c9e3d81f03a9c172201d144a7901260c2a6f7
pguerrero-at-wiris <pablo@wiris.com>
2023-09-28 01:06:19 +0200

    refactor linkTitle with new component. Add rows to navigate through collections

--- src/components/editLinksWindow.vue
+++ src/components/editLinksWindow.vue
@@ -1,16 +1,16 @@
 <template>
-    <v-card-text>
+    <v-card-text class="pb-0">
         <v-form ref="linkForm">
             <v-text-field :rules="validationRuleRequiredField" label="Link*" variant="solo-filled" v-model="currentLink.url"></v-text-field>
             <v-text-field :rules="validationRuleRequiredField" label="Title*" variant="solo-filled" v-model="currentLink.title"></v-text-field>
             <v-textarea label="Notes" variant="solo-filled" v-model="currentLink.notes"></v-textarea>
-            <v-select label="Collection" :items="collections"
+            <v-select class="select_padding__bottom" label="Collection" :items="collections"
                 variant="solo-filled" v-model="currentCollection" item-title="title" return-object :rules="validationRuleRequiredCollection">
             </v-select>    
         </v-form>
     </v-card-text>
 
-    <v-card-actions class="pt-2 me-2 d-flex justify-end">
+    <v-card-actions class="mt-2 me-2 d-flex justify-end">
         <v-btn variant="elevated" color="blue-darken-3" @click="closeEdit">
             CANCEL
         </v-btn>
@@ -40,3 +40,9 @@ const { validationRuleRequiredField, validationRuleRequiredCollection } = utilsS
 const saveButtonLoading: Ref<boolean> = ref(false);
 
 </script>
+
+<style scoped>
+.select_padding__bottom > :deep(.v-input__details) {
+    display: none;
+}
+</style>
