4cf4635ca6dc71fd8876f9f5db3948e6505a7537
pablo-guelu <pablo.guelu@gmail.com>
2024-04-02 02:06:36 +0200

    clean up commit I guess

--- dist/service-worker-loader.js
+++ dist/service-worker-loader.js
@@ -1,1 +1,3 @@
-import './assets/background.ts-4ed993c7.js';
+import 'http://localhost:5173/@vite/env';
+import 'http://localhost:5173/@crx/client-worker';
+import 'http://localhost:5173/src/background.ts';
