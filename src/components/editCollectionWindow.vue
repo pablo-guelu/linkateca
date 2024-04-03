d01924016623bd5ed7937f72bb3e65d100522229
pablo-guelu <pablo.guelu@gmail.com>
2024-04-03 20:15:52 +0200

    update version 0.3.0 manifest (corrections)

--- src/components/editCollectionWindow.vue
+++ src/components/editCollectionWindow.vue
@@ -5,10 +5,10 @@
                 v-model="currentCollection.title"></v-text-field>
             <v-textarea label="Notes" variant="solo-filled" v-model="currentCollection.notes"></v-textarea>
             <v-card-actions class="pt-6 me-2 d-flex justify-end">
-                <v-btn variant="elevated" color="blue-darken-3" @click="closeEdit">
+                <v-btn variant="elevated" :color="appColor" @click="closeEdit">
                     CANCEL
                 </v-btn>
-                <v-btn variant="elevated" color="blue-darken-3" :loading="saveButtonLoading" @click="() => {
+                <v-btn variant="elevated" :color="appColor" :loading="saveButtonLoading" @click="() => {
                     collections = updateCollections(collections, currentCollection.id, currentCollection)
                 }">
                     SAVE
@@ -23,11 +23,18 @@ import { Ref, ref } from 'vue';
 import { useCollectionStore } from '../stores/collections';
 import { storeToRefs } from 'pinia';
 import { useUtilsStore } from '../stores/utils';
+import { useSettingsStore } from '../stores/settings';
 
 const collectionStore = useCollectionStore();
 const { currentCollection, collectionForm, collections } = storeToRefs(collectionStore);
 const { closeEdit, updateCollections } = collectionStore;
+
 const utilsStore = useUtilsStore();
 const { validationRuleRequiredField } = utilsStore;
+
+const settingsStore = useSettingsStore();
+const { appColor } = storeToRefs(settingsStore);
+
 const saveButtonLoading: Ref<boolean> = ref(false);
+
 </script>
--- src/components/editPromptWindow.vue
+++ src/components/editPromptWindow.vue
@@ -8,10 +8,10 @@
     </v-card-text>
 
     <v-card-actions class="pt-6 me-2 d-flex justify-end">
-        <v-btn variant="elevated" color="blue-darken-3" @click="closeEdit">
+        <v-btn variant="elevated" :color="appColor" @click="closeEdit">
             CANCEL
         </v-btn>
-        <v-btn variant="elevated" color="blue-darken-3" :loading="saveButtonLoading" @click="savePrompt">
+        <v-btn variant="elevated" :color="appColor" :loading="saveButtonLoading" @click="savePrompt">
             SAVE
         </v-btn>
     </v-card-actions>
@@ -22,10 +22,14 @@ import { Prompt } from '../types';
 import { Ref, ref } from 'vue';
 import { storeToRefs } from 'pinia';
 import { usePromptStore } from '../stores/prompts';
+import { useSettingsStore } from '../stores/settings';
 
 const promptStore = usePromptStore();
 const {promptForm} = storeToRefs(promptStore);
 const { savePrompt, closeEdit, validationRules } = promptStore;
+
+const settingsStore = useSettingsStore();
+const { appColor } = storeToRefs(settingsStore);
 
 const saveButtonLoading: Ref<boolean> = ref(false);
 
